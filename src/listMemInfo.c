c9cda2856c9c094a054df78b20fb22ebcdfd8ffe
Branden Kim <brandenk0@gmail.com>
2017-11-26 02:16:51 -0500

    Fixed the bugs

--- src/listMemInfo.c
+++ src/listMemInfo.c
@@ -3,6 +3,8 @@
 #include <linux/sched.h>
 #include <linux/syscalls.h>
 
+#include "memInfo.h"
+
 typedef struct memInfo {
     long int pid;
     long int vms;
@@ -11,7 +13,7 @@ typedef struct memInfo {
     long int maj_flt;
 } memInfo;
 
-struct memInfo *listMem[];
+struct memInfo **listMem;
 
 void dfs(struct task_struct *task, int size) {
     struct task_struct *child;
@@ -39,11 +41,11 @@ void dfs(struct task_struct *task, int size) {
     }
 }
 
-asmlinkage long sys_listMemInfo(void **user[], int size) {
+asmlinkage long sys_listMemInfo(void **user, int size) {
     printk("listMemInfo sysCall!\n");
     listMem = (struct memInfo *)user;
 
-    int list_size = sizeof(listMem) / sizeof(struct memInfo *);
+    int list_size = size / sizeof(struct memInfo *);
 
     int i;
     for (i = 0; i < list_size; i++) {
@@ -53,7 +55,7 @@ asmlinkage long sys_listMemInfo(void **user[], int size) {
     dfs(&init_task, list_size);
 
     // Return listMem to user
-    int ret = copy_to_user(user, (void *)listMem, sizeof((struct listMem) * size));
+    int ret = copy_to_user(user, (void *)listMem, sizeof(struct listMem *));
     if (ret) {
         printk("Error! %d\n", ret);
     }
--- src/pp6User.c
+++ src/pp6User.c
@@ -14,26 +14,27 @@ typedef struct memInfo {
 
 int main(int argc, char *argv[]) {
     memInfo **user = malloc(sizeof(struct memInfo *));
-    
+
     // Setup array of memInfo structs from user input
     int i, pid;
-    for (i = 0; i < argc; i++) {
+    for (i = 1; i < argc; i++) {
         pid = atoi(argv[i]);
-        memInfo temp = (memInfo){ pid, 0, 0, 0, 0 };
-        user[i] = &temp;
+        memInfo *temp = malloc(sizeof(struct memInfo));
+        *temp = (memInfo){ pid, 0, 0, 0, 0 };
+        user[i-1] = temp;
     }
 
     // Make syscall
-    long int ret_status = syscall(328, (void *)user, sizeof(struct memInfo *) * argc);
+    long int ret_status = syscall(328, (void *)user, sizeof(struct memInfo *) * (argc - 1));
 
     if (ret_status == 0) {
         memInfo **ret = (struct memInfo *)user;
 
         // Print out each process' memory info
-        for (i = 0; i < argc; i++) {
-            printf("PID: %ld\n", ret[i]->pid);
-            printf("MajFlts: %ld  MinFlts: %ld\n", ret[i]->maj_flt, ret[i]->min_flt);
-            printf("VM: %ld  RSS: %ld\n", ret[i]->vms, ret[i]->rss);
+        for (i = 0; i < argc - 1; i++) {
+            printf("----- PID: %ld -----\n", ret[i]->pid);
+            printf("Major Faults: %ld  Minor Faults: %ld\n", ret[i]->maj_flt, ret[i]->min_flt);
+            printf("VMS: %ld  RSS: %ld\n", ret[i]->vms, ret[i]->rss);
         }
     }
     else {
